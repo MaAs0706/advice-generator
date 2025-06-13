2f4a736e15c7d38b935f3d761db77a519cd30512
MaAs0706e <aswanthmadhav07@gmail.com>
2025-06-14 00:09:35 +0530

    changes

---
+++ script.js
@@ -0,0 +1,3 @@
+const adviceIdElement = document.getElementById('advice-id');
+const adviceTextElement = document.getElementById('advice-text');
+const diceButton = document.querySelector('.dice-button');
