003aaa61823a0fbbbb98a6d58c190547bac760e4
MaAs0706e <aswanthmadhav07@gmail.com>
2025-06-14 06:55:43 +0530

    js addded

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const adviceIdElement = document.getElementById('advice-id');
 const adviceTextElement = document.getElementById('advice-text');
 const diceButton = document.querySelector('.dice-button');
+
+async function fetchadvise (){
+    try{
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        const {id,advice} = data.slip ;
+        adviceIdElement.textContent = id ; 
+        adviceTextElement.textContent = `"${advice}"`;
+       } 
+       catch(error){
+        console.error('Error fetching advice',error);
+        alert('error ,please reload')
+
+       }
+    
+}
